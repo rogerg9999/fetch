10e7cb5e23ab4919d8d046f411f35c9b49e53d98
rogerg9999 <rogerg9999@gmail.com>
2013-10-09 13:31:55 +0200

    second

--- app.js
+++ app.js
@@ -131,6 +131,11 @@ var fetch = function(url, parentSelector, linkSelector, titleSelector, callback)
 	});
 }
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
 
 app.get('/', function(req, res){
 	var query = req.query.q;
