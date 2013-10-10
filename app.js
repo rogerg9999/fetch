d3d12c4286c5dbccb4d1ed9da2e356ae9c5d7c8e
rogerg9999 <rogerg9999@gmail.com>
2013-10-10 22:57:01 +0200

    third

--- app.js
+++ app.js
@@ -137,6 +137,12 @@ app.all('/', function(req, res, next) {
   next();
  });
 
+app.all('/search/:q', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+
 app.get('/', function(req, res){
 	var query = req.query.q;
 	skull(query, function(data){
